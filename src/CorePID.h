2026cced79997e830434742badbd1f6adffe7b1e
Kostas Karouzos <ck.karouzos@gmail.com>
2025-12-26 03:49:16 +0200

    removed deprecated notes

--- src/CorePID.h
+++ src/CorePID.h
@@ -7,24 +7,6 @@
 #include "PIDGains.h"
 
 
-/*
-* https://www.mathworks.com/videos/understanding-pid-control-part-3-expanding-beyond-a-simple-derivative-1531120808026.html
-* https://www.mathworks.com/videos/understanding-pid-control-part-2-expanding-beyond-a-simple-integral-1528310418260.html
-*
-* implements clamping integral anti-windup, derivative on measurement and filtered derivative with cutoff frequency N rad/sec (the version with an integrator in feedback loop instead of a forward derivative, see video)
-* cutoff freq in rad/sec is cutoff freq in Hz * 2 * pi
-*
-* A note about atomicity:
-	getSetPoint() and getMeasurement() use atomic access.
-	compute() also uses atomic access 
-	setSetpoint(), setMEasurement(), incrementSetpoint(), incrementMeasurement() are not atomic.
-	That is only a problem if one of the above is executed outside of an ISR and:
-	- another one of the above affecting the same variable (setpoint or measurement) is also executed inside an ISR, or:
-	- the compute() method is executed inside an ISR (eg. of a timer for timed execution)
-	In the above cases, the above methods should be enclosed by cli(); ... sei();
-*
-*/
-
 #define SCALING_SHIFT  8
 #define SCALING_MULT   (0x1ULL << SCALING_SHIFT) 
 #define PARAM_BITS   16
